18a43d0a1d7c9feecaa741ab188bb23890b146a1
Eric Vere <erkver250@gmail.com>
2019-01-18 12:12:54 -0600

    Completed project

--- index.js
+++ index.js
@@ -2,17 +2,59 @@
 // eg. const includes = () => || function includes() {}
 
 /* includes */
-
+const includes = (arr, item) => {
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] === item) {
+      return true
+    }
+  }
+  return false;
+};
 /* indexOf */
-
+const indexOf = (arr, item) => {
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] === item) {
+      return i;
+    }
+  }
+  return -1;
+};
 /* forEach */
-
+const forEach = (arr, cb) => {
+  for(let i =0; i < arr.length; i++) {
+    arr[i] = cb(arr[i], i, arr)
+  }
+  return arr;
+};
 /* map */
-
+const map = (arr, cb) => {
+  let copy = [];
+  for(let i = 0; i < arr.length; i++) {
+    copy.push(cb(arr[i], i, arr));
+  }
+  return copy;
+};
 /* filter */
-
+const filter = (arr, cb) => {
+  let copy = [];
+  for(let i = 0; i < arr.length; i++) {
+    arr[i] = cb(arr[i], i, arr);
+    if(arr[i] === true) {
+      copy.push(arr[i]);
+    }
+  }
+  return copy;
+};
 /* reduce */
-
+const reduce = (arr, cb, acc) => {
+  if(!acc) {
+    acc = 0;
+  }
+  for(let i = 0; i < arr.length; i++) {
+    acc = cb(acc, arr[i], i, arr);
+  }
+  return acc;
+}
 module.exports = {
   includes,
   indexOf,
